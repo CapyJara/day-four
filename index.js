e567d1c5c2ef2ce522b291739427430b3997ccb3
Jared Myhrberg <jared.myhrberg@gmail.com>
2018-10-04 20:26:20 -0700

    working thing

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 function handleSubmit(event) {
     var form = event.target;
     var elements = form.elements;
-    var name = elements.name.value;
+    var guess = elements.guess.value;
+    var result = document.getElementById('result');
 
-    console.log('name', name);
+    var numAnimals = 4;
+
+    if(guess == numAnimals){
+        result.textContent = 'you guessed right, smawt mutha trucker';
+        console.log("you guessed", guess, ', you guessed right, smawt mutha trucker');
+        }
+    else if(guess < numAnimals) {
+        result.textContent = 'you guessed to low sucka, guess again';
+        console.log("you guessed", guess, ', you guessed to low sucka, guess again');
+        }
+    else {
+        result.textContent = 'you guessed to high silly, do better!';
+        console.log("you guessed", guess, ', you guessed to high silly, do better!');
+        }
+
 }
 
 
